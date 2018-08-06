b60fdf6b51bb566c4dde5f9295a5f37a5368942b
GuillermoElectrico <guillermoelectrico@outlook.com>
2018-08-06 20:02:00 +0200

    Update and fix

--- read_energy_meter.py
+++ read_energy_meter.py
@@ -75,14 +75,14 @@ class DataCollector:
                     while retries > 0:
                         try:
                             retries -= 1
-							if meter['function'] == 3:
+                            if meter['function'] == 3:
                                 if parameters[parameter][2] == 1:
                                     resultado = master.execute(meter['id'], cst.READ_HOLDING_REGISTERS, parameters[parameter][0], parameters[parameter][1], data_format='>f')
                                 elif parameters[parameter][2] == 2:
                                     resultado = master.execute(meter['id'], cst.READ_HOLDING_REGISTERS, parameters[parameter][0], parameters[parameter][1], data_format='>l')
                                 elif parameters[parameter][2] == 3:
                                     resultado = master.execute(meter['id'], cst.READ_HOLDING_REGISTERS, parameters[parameter][0], parameters[parameter][1], data_format='>i')
-							elif meter['function'] == 4:
+                            elif meter['function'] == 4:
                                 if parameters[parameter][2] == 1:
                                     resultado = master.execute(meter['id'], cst.READ_INPUT_REGISTERS, parameters[parameter][0], parameters[parameter][1], data_format='>f')
                                 elif parameters[parameter][2] == 2:
